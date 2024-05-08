03e590ad46757b121ee0e740adaa4b83d32e7aa7
Yacine Boudebouz <yacineboudebbouz7@gmail.com>
2024-05-08 18:55:38 +0200

    fix bugs

--- backend/src/controllers/user_controller.ts
+++ backend/src/controllers/user_controller.ts
@@ -7,8 +7,10 @@ export const getAllUsers = async (req: Request, res: Response) => {
     try {
         const users = await User.find();
         res.status(200).json({ message: "Ok", data: users });
+        return;
     } catch (e) {
         res.status(500).json({ message: "Error", error: e.message });
+        return;
     }
 }
 // login user
@@ -18,16 +20,20 @@ export const userLogin = async (req: Request, res: Response, next: NextFunction)
         const user = await User.findOne({ email })
         if (!user) {
             res.status(404).json({ message: "User not found" });
+            return;
         }
         const isValid = await compare(password, user.password);
         if (!isValid) {
             res.status(403).json({ message: "Invalid password" });
+            return;
         }
 
         res.status(200).json({ message: "OK", id: user._id.toString() });
+        return;
 
     } catch (e) {
         res.status(500).json({ message: "Error", error: e.message });
+        return;
     }
 }
 // creating a new user 
@@ -37,13 +43,16 @@ export const userSignup = async (req: Request, res: Response, next: NextFunction
         const existingUser = await User.findOne({ email });
         if (existingUser) {
             res.status(400).json({ message: "User exists" });
+            return;
         }
         const hashedPassword = await hash(password, 10);
         const user = new User({ name, email, password: hashedPassword });
         await user.save();
         res.status(201).json({ message: "User created", id: user._id.toString() });
+        return;
     } catch (e) {
         res.status(500).json({ message: "Error", error: e.message });
+        return;
     }
 
 }
