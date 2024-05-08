0d3ed08a371bae651760457a1d47a5146399f420
Yacine Boudebouz <yacineboudebbouz7@gmail.com>
2024-05-08 21:52:44 +0200

    frontend

--- backend/src/controllers/user_controller.ts
+++ backend/src/controllers/user_controller.ts
@@ -56,6 +56,17 @@ export const userSignup = async (req: Request, res: Response, next: NextFunction
         const hashedPassword = await hash(password, 10);
         const user = new User({ name, email, password: hashedPassword });
         await user.save();
+
+        res.clearCookie(COOKIE_NAME, { httpOnly: true, path: "/", domain: "localhost", signed: true });
+        // send token to the client with a cookie
+        // we are using a cookie to store the token because we are using httpOnly: true and this will prevent the client from accessing the token
+        const token = createToken(user._id.toString(), user.email, "7d");
+        const expires = new Date();
+        expires.setDate(expires.getDate() + 7);
+        res.cookie(COOKIE_NAME, token, { httpOnly: true, path: "/", domain: "localhost", expires, signed: true });
+        res.status(200).json({ message: "OK", id: user._id.toString() });
+        return;
+
         res.status(201).json({ message: "User created", id: user._id.toString() });
         return;
     } catch (e) {
