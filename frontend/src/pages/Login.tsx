031454d567269acf233c0c799728b31d6539ba28
Yacine Boudebouz <yacineboudebbouz7@gmail.com>
2024-06-12 22:54:25 +0200

    login

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -7,7 +7,24 @@ const Login = () => {
       <Box padding={8} mt={8} display={{ md: "flex", sm: "none", xs: "none" }}>
         <img src="airobot.png" alt="robot" style={{ width: "400px" }} />
       </Box>
-      <Box></Box>
+      <Box
+        display={"flex"}
+        flex={{ xs: 1, md: 0.5 }}
+        justifyContent={"center"}
+        alignItems={"center"}
+        ml={"auto"}
+        mt={26}
+      >
+        <form
+          style={{
+            margin: "auto",
+            padding: "30px",
+            boxShadow: "10px 10px 20px #000",
+            borderRadius: "10px",
+            border: "none",
+          }}
+        ></form>
+      </Box>
     </Box>
   );
 };
